d6fed65b1e307f843646e966b8b8ef4b13952d45
pmpr.dev <pmpr.dev@gmail.com>
2024-10-15 12:27:04 +0000

    Obfuscated

--- src/Pmpr/Cover/Gfan/Widget/Link.php
+++ src/Pmpr/Cover/Gfan/Widget/Link.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670e55b52c7e3             |
+    |             670e5f62f15a7             |
     |_______________________________________|
 */
- namespace Pmpr\Cover\Gfan\Widget; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Common\Foundation\Widget; class Link extends Widget { public function __construct() { parent::__construct(__("\x4c\151\x6e\x6b\163", PR__CVR__GFAN), __("\x44\x69\163\160\154\141\x79\40\x74\x68\145\x20\x73\x65\x6c\145\x63\164\x65\x64\40\154\151\156\153\x73\56", PR__CVR__GFAN)); } public function ykwqaukkycogooii() { $uuyucgkyusckoaeq = $this->caokeucsksukesyo()->wmkogisswkckmeua(); $this->mkksewyosgeumwsa($uuyucgkyusckoaeq->mccagaqeagiikkec(Constants::qwumqqyuasyskkkc)->gswweykyogmsyawy(__("\x49\x74\x65\x6d\x73", PR__CVR__GFAN))->ukqywcsoogkyoaoa()); } }
+ namespace Pmpr\Cover\Gfan\Widget; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Common\Foundation\Widget; class Link extends Widget { public function __construct() { parent::__construct(__("\114\x69\156\x6b\x73", PR__CVR__GFAN), __("\104\x69\163\160\154\x61\x79\x20\x74\x68\x65\x20\x73\x65\154\x65\143\164\145\144\x20\x6c\151\156\x6b\163\x2e", PR__CVR__GFAN)); } public function ykwqaukkycogooii() { $uuyucgkyusckoaeq = $this->caokeucsksukesyo()->wmkogisswkckmeua(); $this->mkksewyosgeumwsa($uuyucgkyusckoaeq->mccagaqeagiikkec(Constants::qwumqqyuasyskkkc)->gswweykyogmsyawy(__("\x49\x74\145\x6d\x73", PR__CVR__GFAN))->ukqywcsoogkyoaoa()); } }
